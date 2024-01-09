82cb1ed2346f886d06c8fbc24d1ab02bd8b5e43f
jjmarin10 <jjmarinva10@gmail.com>
2024-01-09 09:34:21 -0500

    feat: Add ConfigModule service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuitcaseModule } from './modules/suitcases/suitcase.module';
 import { ArticleModule } from './modules/articles/article.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SuitcaseModule, ArticleModule],
+  imports: [SuitcaseModule, ArticleModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   // swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -14,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT',3000));
 }
 bootstrap();
